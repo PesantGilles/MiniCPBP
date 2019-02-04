0b49982a5abe120ee88fd10220c7ce3ff1e5b546
PesantGilles <gilles.pesant@polymtl.ca>
2019-02-04 15:21:48 -0500

    AllDifferentAC constraint is now BP-enabled

--- engine/constraints/AllDifferentAC.java
+++ engine/constraints/AllDifferentAC.java
@@ -11,6 +11,9 @@
  * along with mini-cp. If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
  *
  * Copyright (c)  2018. by Laurent Michel, Pierre Schaus, Pascal Van Hentenryck
+ *
+ * mini-cpbp, replacing classic propagation by belief propagation 
+ * Copyright (c)  2019. by Gilles Pesant
  */
 
 package minicp.engine.constraints;
@@ -19,11 +22,13 @@ import minicp.engine.core.AbstractConstraint;
 import minicp.engine.core.IntVar;
 import minicp.util.GraphUtil;
 import minicp.util.GraphUtil.Graph;
+import minicp.state.StateSparseSet;
 import minicp.util.exception.InconsistencyException;
-import minicp.util.exception.NotImplementedException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Arc Consistent AllDifferent Constraint
@@ -67,30 +72,92 @@ public class AllDifferentAC extends AbstractConstraint {
     private int minVal;
     private int maxVal;
 
+    private static final int exactPermanentThreshold = 6;
+    private double[][] beliefs;
+    private StateSparseSet freeVars; // holds an index for vars
+    private StateSparseSet freeVals; // holds the actual values of freeVals
+    private double[] gamma;
+    private int[] c;
+    private int[] permutation;
+    private int[] varIndices;
+    private int[] vals;
+
     public AllDifferentAC(IntVar... x) {
         super(x);
         this.x = x;
         maximumMatching = new MaximumMatching(x);
         match = new int[x.length];
         this.nVar = x.length;
+
+	freeVars = new StateSparseSet(getSolver().getStateManager(),x.length,0);
+	// accumulate values from domains
+	SortedSet<Integer> allVals = new TreeSet<Integer>();
+	for(IntVar var : x) {
+	    int s = var.fillArray(domainValues);
+	    for (int j = 0; j < s; j++) {
+		allVals.add(domainValues[j]);
+	    }
+	}
+	// remove assigned variables and their values from further consideration
+	for(int i = 0; i < x.length; i++) {
+	    if (x[i].isBound()){
+		freeVars.remove(i);
+		int val = x[i].min();
+		allVals.remove(val);
+		// apply basic fwd checking (because we may not call propagate())
+		for (int k = 0; k < i; k++){
+		    x[k].remove(val);
+		}
+		for (int k = i+1; k < x.length; k++){
+		    x[k].remove(val);
+		}
+	    }
+	}
+	freeVals = new StateSparseSet(getSolver().getStateManager(),allVals.last().intValue()-allVals.first().intValue()+1,allVals.first().intValue());	
+	// remove missing intermediate values from interval domain
+	for (int i = allVals.first().intValue()+1; i < allVals.last().intValue(); i++){
+	    if (!allVals.contains(i))
+		freeVals.remove(i);
+	} // from now on freeVals will be maintained as a superset of the available values, 
+	  // only removing values as they are taken on by a variable
+
+	// allocate enough space for the data structures, even though we will need less and less as we go down the search tree
+	beliefs = new double[freeVals.size()][freeVals.size()];
+	c = new int[freeVals.size()-1];
+	permutation = new int[freeVals.size()-1];
+	varIndices = new int[freeVars.size()];
+	vals = new int[freeVals.size()];
+	if (freeVals.size()-1 <= exactPermanentThreshold){
+	    setExactWCounting(true);
+	}
+	else{
+	    setExactWCounting(false); // actually, it will be exact below the threshold, which may happen lower in the search tree
+	    precompute_gamma(freeVals.size()-1);
+	}
     }
 
     @Override
     public void post() {
-        for (int i = 0; i < nVar; i++) {
-            x[i].propagateOnDomainChange(this);
-        }
-        updateRange();
-
-        matched = new boolean[nVal];
-        nNodes = nVar + nVal + 1;
-        in = new ArrayList[nNodes];
-        out = new ArrayList[nNodes];
-        for (int i = 0; i < nNodes; i++) {
-            in[i] = new ArrayList<>();
-            out[i] = new ArrayList<>();
-        }
-        propagate();
+	switch(getSolver().getMode()) {
+	case BP:
+	    if (isExactWCounting())
+		break; // only schedule propagate() if counting is not exact
+	case SP:
+	case SBP:
+	    for (int i = 0; i < nVar; i++) {
+ 		x[i].propagateOnDomainChange(this);
+	    }
+	    updateRange();
+	    matched = new boolean[nVal];
+	    nNodes = nVar + nVal + 1;
+	    in = new ArrayList[nNodes];
+	    out = new ArrayList[nNodes];
+	    for (int i = 0; i < nNodes; i++) {
+		in[i] = new ArrayList<>();
+		out[i] = new ArrayList<>();
+	    }
+	    propagate();
+	}
     }
 
     private void updateRange() {
@@ -111,19 +178,237 @@ public class AllDifferentAC extends AbstractConstraint {
             in[i].clear();
             out[i].clear();
         }
-        // TODO continue the implementation for representing the residual graph
-         throw new NotImplementedException("AllDifferentAC");
+        Arrays.fill(matched, 0, nVal, false);
+        for (int i = 0; i < x.length; i++) {
+            in[i].add(match[i] - minVal + x.length);
+            out[match[i] - minVal + nVar].add(i);
+            matched[match[i] - minVal] = true;
+        }
+        for (int i = 0; i < nVar; i++) {
+            for (int v = x[i].min(); v <= x[i].max(); v++) {
+                if (x[i].contains(v) && match[i] != v) {
+                    in[v - minVal + nVar].add(i);
+                    out[i].add(v - minVal + nVar);
+                }
+            }
+        }
+        for (int v = minVal; v <= maxVal; v++) {
+            if (!matched[v - minVal]) {
+                in[sink].add(v - minVal + nVar);
+                out[v - minVal + nVar].add(sink);
+            } else {
+                in[v - minVal + nVar].add(sink);
+                out[sink].add(v - minVal + nVar);
+            }
+        }
     }
 
 
     @Override
     public void propagate() {
-        // TODO Implement the filtering
-        // hint: use maximumMatching.compute(match) to update the maximum matching
-        //       use updateRange() to update the range of values
-        //       use updateGraph() to update the residual graph
-        //       use  GraphUtil.stronglyConnectedComponents to compute SCC's
-         throw new NotImplementedException("AllDifferentAC");
+        // update the maximum matching
+        int size = maximumMatching.compute(match);
+        if (size < x.length) {
+            throw new InconsistencyException();
+        }
+        // update the range of values
+        updateRange();
+        // update the residual graph
+        updateGraph();
+        // compute SCC's
+        int[] scc = GraphUtil.stronglyConnectedComponents(g);
+        for (int i = 0; i < nVar; i++) {
+            for (int v = minVal; v <= maxVal; v++) {
+                    if (match[i] != v && scc[i] != scc[v - minVal + nVar]) {
+                        x[i].remove(v);
+                    }
+            }
+        }
+    }
+
+    @Override
+    public void updateBelief(){
+	int nbVar, nbVal;
+	// update freeVars/Vals according to bound variables
+	nbVar = freeVars.fillArray(varIndices);
+	for (int j = 0; j < nbVar; j++) {
+	    int i = varIndices[j];
+	    if (x[i].isBound()) {
+		freeVars.remove(i);
+		int val = x[i].min();
+		freeVals.remove(val);
+		// set trivial local belief for bound var...
+		setLocalBelief(i,val,1);
+		// ...and for other vars being assigned that val
+		for (int k = 0; k < j; k++)
+		    if (x[varIndices[k]].contains(val))
+			setLocalBelief(varIndices[k],val,0);
+		for (int k = j+1; k < nbVar; k++)
+		    if (x[varIndices[k]].contains(val))
+			setLocalBelief(varIndices[k],val,0);
+	    }
+	}
+ 	// initialize outside beliefs matrix
+	nbVar = freeVars.fillArray(varIndices);
+	nbVal = freeVals.fillArray(vals);
+	for (int j = 0; j < nbVar; j++) {
+	    int i = varIndices[j];
+	    for (int k = 0; k < nbVal; k++) {
+		int val = vals[k];
+		beliefs[j][k] = (x[i].contains(val) ? 
+				 outsideBelief(i,val):
+				 0);
+	    }
+	}
+	// may need to add dummy rows in order to make the beliefs matrix square
+	for (int j = 0; j < nbVal - nbVar; j++) { 
+	    for (int k = 0; k < nbVal; k++) {
+		beliefs[nbVar+j][k] = 1;
+	    }
+	}
+	// set local beliefs by computing the permanent of beliefs sub-matrices
+	if (nbVal-1 <= exactPermanentThreshold) {
+	    // exact permanent
+	    for (int j = 0; j < nbVar; j++) {
+		int i = varIndices[j];
+		for (int k = 0; k < nbVal; k++) {
+		    int val = vals[k];
+		    if(x[i].contains(val)) {
+			// note: will be normalized later in AbstractConstraint.sendMessages()
+    			setLocalBelief(i,val,costBasedPermanent_exact(j,k,beliefs,nbVal));
+		    }
+		}
+	    }		
+	}
+	else {
+	    // approximate permanent
+	    for (int j = 0; j < nbVar; j++) {
+		int i = varIndices[j];
+		for (int k = 0; k < nbVal; k++) {
+		    int val = vals[k];
+		    if(x[i].contains(val)) {
+			// note: will be normalized later in AbstractConstraint.sendMessages()
+    			setLocalBelief(i,val,costBasedPermanent_UB3(j,k,beliefs,nbVal));
+		    }
+		}
+	    }
+	}
+    }
+
+    // precompute gamma function up to n
+    private void precompute_gamma(int n) {
+	double factorial=1.0;
+	gamma = new double[n+1];
+	gamma[0]=1.0;
+	for(int i=1; i<=n; i++){
+	    factorial *= (double) i;
+	    gamma[i] = Math.pow( factorial, 1.0/((double) i));
+	}
+    }
+
+    private double costBasedPermanent_UB3(int var, int val, double[][] m, int dim){
+	// permanent upper bound U^3 for nonnegative matrices (from Soules 2003) 
+	// for matrix m without row of var and column of val
+	double U3 = 1.0; 
+	double rowSum, rowMax, tmp;
+	int tmpFloor, tmpCeil;
+
+	for(int i=0; i<dim; i++){
+	    if(i!=var){ // exclude row of var whose belief we are computing
+		rowSum = rowMax = 0;
+		for(int j=0; j<dim; j++){
+		    tmp = m[i][j];
+		    if(j!=val){ // exclude column of val whose belief we are computing
+			rowSum += tmp;
+			if(tmp > rowMax)
+			    rowMax = tmp;
+		    }
+		}
+		if(rowMax == 0)
+		    return 0;
+		tmp = rowSum/rowMax;
+		tmpFloor = (int) Math.floor(tmp);
+		tmpCeil = (int) Math.ceil(tmp);
+		U3 *= rowMax * (gamma[tmpFloor] + (tmp-tmpFloor)*(gamma[tmpCeil]-gamma[tmpFloor]));
+	    }
+	}
+	return U3; // that upper bound should actually be divided by (# dummy rows)!
+    }
+
+    private double costBasedPermanent_exact(int var, int val, double[][] m, int dim){
+	// exact permanent for matrix m without row of var and column of val
+	// to be used when m is not too large
+
+	double tmp;
+
+	// swap row "var" and column "val" with last row & column
+	for(int j=0; j<dim; j++){ // swap rows 
+	    tmp=m[var][j]; m[var][j]=m[dim-1][j]; m[dim-1][j]=tmp;
+	}
+	for(int i=0; i<dim; i++){ // swap columns 
+	    tmp=m[i][val]; m[i][val]=m[i][dim-1]; m[i][dim-1]=tmp;
+	}
+
+	// compute permanent of m without last row & column
+	double p = permanent(m, dim-1);
+
+	// swap back
+	for(int j=0; j<dim; j++){ // swap rows 
+	    tmp=m[var][j]; m[var][j]=m[dim-1][j]; m[dim-1][j]=tmp;
+	}
+	for(int i=0; i<dim; i++){ // swap columns 
+	    tmp=m[i][val]; m[i][val]=m[i][dim-1]; m[i][dim-1]=tmp;
+	}
+
+	return p; // that value should actually be divided by (# dummy rows)!
+    }
+
+    // compute the permanent of a real matrix through a simple adaptation of Heap's Algorithm that generates all permutations
+    private double permanent(double[][] A, int n) {
+	double prod = 1.0;
+	for(int i=0; i<n; i++){
+	    c[i]=0;
+	    permutation[i]=i;
+	    prod *= A[i][i];
+	}
+	double perm = prod;
+	int i=0;
+	while (i<n){
+	    if  (c[i]<i){
+		if (i%2==0)
+		    prod = swap(A, permutation, 0, i, n, prod);
+		else
+		    prod = swap(A, permutation, c[i], i, n, prod);
+		perm += prod;
+		c[i]++;
+		i=0;
+	    }
+	    else{
+		c[i]=0;
+		i++;
+	    }
+	}
+	return perm;
+    }
+    
+    // swap the elements at indices i and j in the permutation for Heap's algorithm
+    // returns the new prod   
+    private double swap(double[][] A, int[] permutation, int i, int j, int n, double prod) {
+        int e = permutation[i];
+        permutation[i] = permutation[j];
+        permutation[j] = e;
+	double newFactor = A[i][permutation[i]]*A[j][permutation[j]];
+	double oldFactor = A[i][permutation[j]]*A[j][permutation[i]];
+	if (newFactor == 0)
+	    return 0;
+	else if (oldFactor == 0) {
+	    // cannot divide it out -- compute from scratch
+	    double newProd = 1.0;
+	    for(int k=0; k<n; k++)
+		newProd *= A[k][permutation[k]];
+	    return newProd;
+	} else
+	    return prod*newFactor/oldFactor;
     }
 
 }
