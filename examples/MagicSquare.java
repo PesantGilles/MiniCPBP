2c958a31cacf25bcca2c315be4027822d7f0379a
PesantGilles <gilles.pesant@polymtl.ca>
2019-10-03 12:11:02 -0400

    changed MagicSquare example to partially-filled version and added instances

--- examples/MagicSquare.java
+++ examples/MagicSquare.java
@@ -10,78 +10,54 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with mini-cp. If not, see http://www.gnu.org/licenses/lgpl-3.0.en.html
  *
- * Copyright (c)  2018. by Laurent Michel, Pierre Schaus, Pascal Van Hentenryck
+ * Copyright (c)  2017. by Laurent Michel, Pierre Schaus, Pascal Van Hentenryck
+ *
+ * mini-cpbp, replacing classic propagation by belief propagation 
+ * Copyright (c)  2019. by Gilles Pesant
  */
 
 package minicp.examples;
 
 
-import minicp.cp.Factory;
 import minicp.engine.core.IntVar;
 import minicp.engine.core.Solver;
 import minicp.search.DFSearch;
 import minicp.search.SearchStatistics;
-
-import java.util.Arrays;
-
-import static minicp.cp.BranchingScheme.firstFail;
 import static minicp.cp.Factory.*;
+import static minicp.cp.BranchingScheme.*;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
- * The Magic Square problem.
+ * The Magic Square Completion problem.
  * <a href="http://csplib.org/Problems/prob019/">CSPLib</a>.
  */
 public class MagicSquare {
 
-    //
     public static void main(String[] args) {
 
-        int n = 6;
-        int sumResult = n * (n * n + 1) / 2;
+	int n = Integer.parseInt(args[0]);
+	int nbFilled = Integer.parseInt(args[1]);
+	int nbFile = Integer.parseInt(args[2]);
 
-        Solver cp = Factory.makeSolver();
-        IntVar[][] x = new IntVar[n][n];
+	boolean notEqual = false;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                x[i][j] = makeIntVar(cp, 1, n * n);
-            }
-        }
+	Solver cp = makeSolver();
 
+	IntVar[] xFlat = makeMagicSquare(cp,n,notEqual,nbFilled,nbFile);
 
-        IntVar[] xFlat = new IntVar[x.length * x.length];
-        for (int i = 0; i < x.length; i++) {
-            System.arraycopy(x[i], 0, xFlat, i * x.length, x.length);
-        }
+	IntVar[][] x = new IntVar[n][n];
+	for(int i = 0; i<n; i++){
+	    for(int j = 0; j<n; j++){
+		x[i][j] = xFlat[i*n+j];
+	    }
+	}
 
-
-        // AllDifferent
-        cp.post(allDifferent(xFlat));
-
-        // Sum on lines
-        for (int i = 0; i < n; i++) {
-            cp.post(sum(x[i], sumResult));
-        }
-
-        // Sum on columns
-        for (int j = 0; j < x.length; j++) {
-            IntVar[] column = new IntVar[n];
-            for (int i = 0; i < x.length; i++)
-                column[i] = x[i][j];
-            cp.post(sum(column, sumResult));
-        }
-
-        // Sum on diagonals
-        IntVar[] diagonalLeft = new IntVar[n];
-        IntVar[] diagonalRight = new IntVar[n];
-        for (int i = 0; i < x.length; i++) {
-            diagonalLeft[i] = x[i][i];
-            diagonalRight[i] = x[n - i - 1][i];
-        }
-        cp.post(sum(diagonalLeft, sumResult));
-        cp.post(sum(diagonalRight, sumResult));
-
-        DFSearch dfs = makeDfs(cp, firstFail(xFlat));
+//    	DFSearch dfs = makeDfs(cp, firstFailRandomVal(xFlat));
+   	DFSearch dfs = makeDfs(cp, maxMarginalStrength(xFlat));
 
         dfs.onSolution(() -> {
                     for (int i = 0; i < n; i++) {
@@ -93,6 +69,78 @@ public class MagicSquare {
         SearchStatistics stats = dfs.solve(stat -> stat.numberOfSolutions() >= 1); // stop on first solution
 
         System.out.println(stats);
+
     }
+
+	public static void partialAssignments(IntVar[][] vars, int n, int nbFilled, int nbFile){
+	    try {
+		Scanner scanner = new Scanner(new FileReader("minicp/examples/data/MagicSquare/magicSquare"+n+"-filled"+nbFilled+"-"+nbFile+".dat"));
+		
+		scanner.nextInt();
+		scanner.nextInt();
+		
+		while(scanner.hasNextInt()){
+		    int row = scanner.nextInt()-1;
+		    int column = scanner.nextInt()-1;
+		    int value = scanner.nextInt();
+		    vars[row][column].assign(value);
+		}
+		scanner.close();
+	    }
+	    catch (IOException e) {
+		System.err.println("Error : " + e.getMessage()) ;
+		System.exit(2) ;
+	    }
+	}
+
+	public static IntVar[] makeMagicSquare(Solver cp, int n, boolean notEqual, int nbFilled, int nbFile) {
+		int M = n*(n*n+1)/2;
+		IntVar[][] x = new IntVar[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				x[i][j] = makeIntVar(cp, 1, n*n);
+				x[i][j].setName("x"+"["+(i+1)+","+(j+1)+"]");
+			}
+		}
+
+		IntVar[] xFlat = new IntVar[x.length * x.length];
+		for (int i = 0; i < x.length; i++) {
+			System.arraycopy(x[i],0,xFlat,i * x.length,x.length);
+		}
+
+		partialAssignments(x,n,nbFilled,nbFile);
+
+		// Sum on lines
+		for (int i = 0; i < n; i++) {
+			cp.post(sum(x[i],M));
+		}
+
+		// Sum on columns
+		for (int j = 0; j < x.length; j++) {
+			IntVar[] column = new IntVar[n];
+			for (int i = 0; i < x.length; i++)
+				column[i] = x[i][j];
+			cp.post(sum(column,M));
+		}
+
+		// Sum on diagonals
+		IntVar[] diagonalLeft = new IntVar[n];
+		IntVar[] diagonalRight = new IntVar[n];
+		for (int i = 0; i < x.length; i++){
+			diagonalLeft[i] = x[i][i];
+			diagonalRight[i] = x[n-i-1][i];
+		}
+		cp.post(sum(diagonalLeft, M));
+		cp.post(sum(diagonalRight, M));
+
+		// AllDifferent
+		if(notEqual)
+ 		    cp.post(allDifferent(xFlat));
+		else
+		    cp.post(allDifferentAC(xFlat));
+
+		return xFlat;
+	}
 
 }
