6588d1ef746641af893c10a6f0e6eb031197564f
PesantGilles <gilles.pesant@polymtl.ca>
2020-01-31 21:28:33 -0500

    added weighted counting for LessOrEqual constraint

--- engine/constraints/LessOrEqual.java
+++ engine/constraints/LessOrEqual.java
@@ -30,12 +30,19 @@ public class LessOrEqual extends AbstractConstraint { // x <= y
         super(vars);
         this.x = x;
         this.y = y;
+   	setExactWCounting(true);
     }
 
     @Override
     public void post() {
-        x.propagateOnBoundChange(this);
-        y.propagateOnBoundChange(this);
+	switch(getSolver().getMode()) {
+	case BP:
+	    break; 
+	case SP:
+	case SBP:
+	    x.propagateOnBoundChange(this);
+	    y.propagateOnBoundChange(this);
+	}
         propagate();
     }
 
@@ -47,4 +54,33 @@ public class LessOrEqual extends AbstractConstraint { // x <= y
             setActive(false);
     }
 
+    @Override
+    public void updateBelief() {
+	double belief;
+	int vx, vy;
+	// Treatment of x
+	belief = beliefRep.zero();
+	vy = y.max();
+	for (vx = x.max(); vx >= x.min(); vx--) {
+	    if (x.contains(vx)) {
+		while ((vx <= vy) && (vy >= y.min())) {
+		    belief = beliefRep.add(belief,outsideBelief(1,vy));
+		    do vy--; while ( !y.contains(vy) && (vy >= y.min()));
+		}
+		setLocalBelief(0,vx,belief);
+	    }
+	}
+	// Treatment of y
+	belief = beliefRep.zero();
+	vx = x.min();
+	for (vy = y.min(); vy <= y.max(); vy++) {
+	    if (y.contains(vy)) {
+		while ((vx <= vy) && (vx <= x.max())) {
+		    belief = beliefRep.add(belief,outsideBelief(0,vx));
+		    do vx++; while ( !x.contains(vx) && (vx <= x.max()));
+		}
+		setLocalBelief(1,vy,belief);
+	    }
+	}
+    }
 }
