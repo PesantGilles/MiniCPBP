03627e492497be415ca3427f983d2e782d9b860b
PesantGilles <gilles.pesant@polymtl.ca>
2021-02-14 22:53:32 -0500

    added Absolute constraint

--- engine/constraints/Absolute.java
+++ engine/constraints/Absolute.java
@@ -40,15 +40,53 @@ public class Absolute extends AbstractConstraint {
     }
 
     public void post() {
-        // TODO
-         throw new NotImplementedException("Absolute");
+        y.removeBelow(0);
+        x.propagateOnBoundChange(this);
+        y.propagateOnBoundChange(this);
+        propagate();
     }
 
     @Override
     public void propagate() {
         // y = |x|
-        // TODO
-         throw new NotImplementedException("Absolute");
+        if (x.isBound()) {
+            y.assign(Math.abs(x.min()));
+            setActive(false);
+        } else if (y.isBound()) { // y is bound
+            // y = |x|
+            if (!x.contains(-y.min())) {
+                x.assign(y.min());
+            } else if (!x.contains(y.min())) {
+                x.assign(-y.min());
+            } else {
+                // x can be (y or -y)
+                // remove everything except y and -y from x
+                for (int v = x.min(); v <= x.max(); v++) {
+                    if (v != y.min() && v != -y.min()) {
+                        x.remove(v);
+                    }
+                }
+            }
+            setActive(false);
+        } else if (x.min() >= 0) {
+            y.removeBelow(x.min());
+            y.removeAbove(x.max());
+            x.removeBelow(y.min());
+            x.removeAbove(y.max());
+        } else if (x.max() <= 0) {
+            y.removeBelow(-x.max());
+            y.removeAbove(-x.min());
+            x.removeBelow(-y.max());
+            x.removeAbove(-y.min());
+        } else {
+            int maxAbs = Math.max(x.max(), -x.min());
+            y.removeAbove(maxAbs);
+            x.removeAbove(y.max());
+            x.removeBelow(-y.max());
+            while (!x.contains(y.min()) && !x.contains(-y.min())) {
+                y.remove(y.min());
+            }
+        }
     }
 
 }
